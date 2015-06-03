d1f55e7f5d57edaa213eaa2ddba10eb0593afb4e
sammcgrail <sammcgrail@gmail.com>
2015-06-03 12:01:50 -0400

    first test works

--- lib/airplane.rb
+++ lib/airplane.rb
@@ -1,2 +1,8 @@
 class Airplane
+  attr_reader :type, :wing_loading, :horsepower
+  def initialize (type, wing_loading, horsepower)
+    @type = type
+    @wing_loading = wing_loading
+    @horsepower = horsepower
+  end
 end
--- spec/lib/airplane_spec.rb
+++ spec/lib/airplane_spec.rb
@@ -2,6 +2,10 @@ require_relative "../../lib/airplane"
 
 describe Airplane do
   describe "#initialization" do
+    it 'assigns values to the variables of an Airplane class instance' do
+      my_plane = Airplane.new("cesna", 10, 150)
+      expect(my_plane.type).to eq("NOTPLANE")
+    end
   end
 
   describe "#land" do
