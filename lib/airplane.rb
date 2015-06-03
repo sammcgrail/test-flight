f7852d2106ee8dd19428eb7459fb00d22bf82e38
sammcgrail <sammcgrail@gmail.com>
2015-06-03 12:18:06 -0400

    start tests writter

--- lib/airplane.rb
+++ lib/airplane.rb
@@ -1,8 +1,26 @@
 class Airplane
   attr_reader :type, :wing_loading, :horsepower
-  def initialize (type, wing_loading, horsepower)
+  def initialize (type, wing_loading, horsepower, engine_on = false)
     @type = type
     @wing_loading = wing_loading
     @horsepower = horsepower
+    @engine_on = engine_on
+  end
+
+  def start
+    if @engine_on == false
+      @engine_on = true
+      "Initial engine start"
+    else
+      "Engine already running"
+    end
+  end
+
+  def takeoff
+    #pseudo
+  end
+
+  def landing
+    #pseudo
   end
 end
--- spec/lib/airplane_spec.rb
+++ spec/lib/airplane_spec.rb
@@ -1,19 +1,42 @@
 require_relative "../../lib/airplane"
 
 describe Airplane do
-  describe "#initialization" do
-    it 'assigns values to the variables of an Airplane class instance' do
-      my_plane = Airplane.new("cesna", 10, 150)
-      expect(my_plane.type).to eq("NOTPLANE")
-    end
-  end
+  # describe "#initialization" do
+  #   it 'assigns value to the type variable of an Airplane class instance' do
+  #     my_plane = Airplane.new("cesna", 10, 150)
+  #     expect(my_plane.type).to eq("NOTPLANE")
+  #   end
+  #
+  #   it 'assigns value to the wing_loading variable of an Airplane class instance' do
+  #     my_plane = Airplane.new("cesna", 10, 150)
+  #     expect(my_plane.wing_loading).to eq(10)
+  #   end
+  #
+  #   it 'assigns value to the horsepower variable of an Airplane class instance' do
+  #     my_plane = Airplane.new("cesna", 10, 150)
+  #     expect(my_plane.horsepower).to eq(150)
+  #   end
+  # end
 
-  describe "#land" do
+  describe "#start" do
+    it 'returns initial engine start response if engine is off' do
+      my_plane = Airplane.new("cesna", 10, 150)
+      expect(my_plane.start).to eq("Initial engine start")
+    end
+    it 'returns engine already running response if engine is on' do
+      my_plane = Airplane.new("cesna", 10, 150)
+      my_plane.start
+      expect(my_plane.start).to eq("Engine already running")
+    end
   end
 
   describe "#takeoff" do
   end
 
-  describe "#start" do
+  describe "#land" do
   end
+
+
+
+
 end
